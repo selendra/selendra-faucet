9f28179114afaec0a242f1b4f8cc5c9daa94e9ec
combo <combo@combos-MacBook-Pro.local>
2021-04-24 12:40:17 +0700

    ui

--- src/pages/Invitation/_id.js
+++ src/pages/Invitation/_id.js
@@ -1,11 +1,14 @@
 import { Row, Col, Card, Input, Button, message } from 'antd';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { useState } from 'react';
 import ShareOnSocial from '../../assets/share.png';
 import { ethers } from 'ethers';
 
 import { ReactComponent as Facebook } from '../../assets/cfacebook.svg';
 import { ReactComponent as Twitter } from '../../assets/ctwitter.svg';
+import { ReactComponent as LinkedIn } from '../../assets/clinkedin.svg';
+import { ReactComponent as Telegram } from '../../assets/ctelegram.svg';
+import { ReactComponent as Copy } from '../../assets/copy.svg';
 
 function Invitation() {
   const cardStyle = {
@@ -17,6 +20,18 @@ function Invitation() {
   const [address, setAddress] = useState('');
   const [visible, setVisible] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  const onCopy = () => {
+    /* Get the text field */
+    var copyText = document.getElementById("receive__wallet");
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+  }
 
   const twelveDigit = (str) => {
     return str.slice(-12);
@@ -43,6 +58,14 @@ function Invitation() {
     window.open(`http://www.facebook.com/sharer.php?u=${refLink}&quote=${shareMSG}`, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600')
   }
 
+  const onLinkedin = () => {
+    window.open(`https://www.linkedin.com/shareArticle?mini=true&url=${refLink}`, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600')
+  }
+
+  const onTelegram = () => {
+    window.open(`https://t.me/share/url?url=${refLink}&text=${shareMSG}`, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600')
+  }
+
   return (
     <div className='intro'>
       <div className='intro__container'>
@@ -67,32 +90,38 @@ function Invitation() {
             { visible && (
             <Row>
               <Card style={cardStyle}>
+                <p className='ref__title'>Share on social:</p><br/>
+                <Row>
+                  <Twitter style={{cursor: 'pointer'}} onClick={onTwitter} />
+                  <Facebook style={{cursor: 'pointer', margin: '0 10px'}} onClick={onFacebook} />
+                  <LinkedIn style={{cursor: 'pointer'}} onClick={onLinkedin} />
+                  <Telegram style={{cursor: 'pointer', margin: '0 10px'}} onClick={onTelegram} />
+                </Row>
+                <div style={{padding: '1rem 0'}}/>
                 <Row>
                   <Col span={12}>
                     <p className='ref__title'>Default Link:</p>
                   </Col>
                   <Col span={12}>
-                    <p className='ref__id'>
-                      {`${window.location.protocol}//${window.location.hostname}${ window.location.port ? ':' : ''}${ window.location.port ? window.location.port : ""}/claim-$sel?ref=${twelveDigit(address)}`}
-                    </p>
+                    <Row>
+                      <p className='ref__id'>
+                        {refLink} 
+                        <input type="text" id="receive__wallet" value={refLink} />
+                      </p>
+                      <Copy onClick={onCopy} style={{color: '#fff', cursor: 'pointer'}}/>
+                    </Row>
                   </Col>
                 </Row>
                 <div style={{padding: '1rem 0'}}/>
                 <Row>
                   <Col span={12}>
                     <p className='ref__title'>You Receive</p><br/>
-                    <p className='ref__id'>+5 $SEL</p>
+                    <p className='ref__id'>+3 $SEL</p>
                   </Col>
                   <Col span={12}>
                     <p className='ref__title'>Friend Receive</p><br/>
-                    <p className='ref__id'>0 $SEL</p>
+                    <p className='ref__id'>+3 $SEL</p>
                   </Col>
-                </Row>
-                <div style={{padding: '1rem 0'}}/>
-                <p className='ref__title'>Share on social:</p><br/>
-                <Row>
-                  <Twitter style={{cursor: 'pointer'}} onClick={onTwitter} />
-                  <Facebook style={{cursor: 'pointer', margin: '0 10px'}} onClick={onFacebook} />
                 </Row>
               </Card>
             </Row>
